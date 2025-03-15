04f2059fe6d92622aa27e69c393245210a3cffd0
RamiSwa <118728140+RamiSwa@users.noreply.github.com>
2025-03-16 00:07:54 +0200

    Configured WhiteNoise for static file storage & updated requirements

--- farming_ai/settings.py
+++ farming_ai/settings.py
@@ -85,6 +85,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.contrib.sessions.middleware.SessionMiddleware',
 
     # CORS Middleware (should be near the top)
@@ -241,14 +242,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Static Files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # For additional static files (optional)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production use
 
+# WhiteNoise Storage for static files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+# Media Files (for user uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
