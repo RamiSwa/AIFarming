98e452c01140671d2041358a2654aa9ed33ca571
RamiSwa <118728140+RamiSwa@users.noreply.github.com>
2025-03-17 08:20:26 +0200

    Fix 404 page 8

--- farming_ai/views.py
+++ farming_ai/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def custom_404_view(request, exception):
     """Handles 404 errors with a custom template."""
-    return render(request, "/templates/pages/404.html", status=404)
+    return render(request, "pages/404.html", status=404)
