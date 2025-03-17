b9710262976df663338d99954e844ecedd914da5
RamiSwa <118728140+RamiSwa@users.noreply.github.com>
2025-03-17 03:42:03 +0200

    update code for PDF one-payment4

--- monetization/services/email_service.py
+++ monetization/services/email_service.py
@@ -85,7 +85,8 @@ def send_report_email(report_request, user, location, top_crop, pdf_url):
         logger.error(f"❌ Failed to download report from {pdf_url} (Status {response.status_code})")
 
     email.send()
-    logger.info(f"📧 Soil Report Email sent to {user.email}" with PDF: {pdf_url}")
+    logger.info(f"📧 Sending soil report to {user.email} with PDF: {pdf_url}")
+
 
 
 def send_order_email(order, user):
